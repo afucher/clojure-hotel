04b37811244322aed4f04c3bb89d86586e0d80c8
Arthur Fücher <arthurfucher@gmail.com>
2019-09-30 21:55:24 -0300

    adiciona mais hoteis

--- src/clojure_hotel/db.clj
+++ src/clojure_hotel/db.clj
@@ -1,4 +1,14 @@
 (ns clojure-hotel.db)
 
-(def lakewood 
-    {:regular {:week 110 :weekend 80}})
+(def lakewood { :regular {:week 110 :weekend 80}
+                :reward  {:week 80  :weekend 80}})
+
+
+(def bridgewood { :regular {:week 160 :weekend 60}
+                  :reward  {:week 110 :weekend 50}})
+
+
+(def lakewood { :regular {:week 220 :weekend 150}
+                :reward  {:week 100  :weekend 40}})
+
+(defn hoteis [lakewood bridgewood lakewood])
